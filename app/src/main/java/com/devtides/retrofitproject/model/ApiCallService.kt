6d9834f71305749117bda1d23711388464daeff4
Gilian Marques Fernandes <gilian762@gmail.com>
2025-04-24 21:45:32 -0300

    Aula 3-4:
    Aprendemos sobre Loggin (para fins de depuração)
    Intercept requests and responses and log them to the console
    Levels:
    • BASIC: request and response lines
    • HEADERS: ↑ + headers
    • BODY: ↑ + bodies
    • NONE
    
    A ApiCallService Foi modificada para incluir um objeto de login dentro do retrofit através do OKHttpclient

--- app/src/main/java/com/devtides/retrofitproject/model/ApiCallService.kt
+++ app/src/main/java/com/devtides/retrofitproject/model/ApiCallService.kt
@@ -1,17 +1,30 @@
 package com.devtides.retrofitproject.model
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiCallService {
 
-    private val BASE_URL = "https://postman-echo.com/"
+    private const val BASE_URL = "https://postman-echo.com/"
 
-    private val api: ApiCall = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-        .create(ApiCall::class.java)
+    private val okHttpClient = OkHttpClient.Builder()
+
+    private val api: ApiCall by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient.build())
+            .build()
+            .create(ApiCall::class.java)
+    }
+
+    init {
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.HEADERS
+        okHttpClient.addInterceptor(logging)
+    }
 
     fun call() = api.callGet()
 
